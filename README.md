516c6cb64c2249f7fb07f137eb18bae8ed773d76
Tyghe Vallard <tyghe@lorex.(none)>
2012-11-24 10:16:51 -0600

    Check for http v1.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Very basic segmented downloader implemented using curl.
 
 # Usage
 
-#> dl.sh <someurl>
+#> dl.sh "<someurl>"
 
 Will segment the download into 10 pieces and download each one concurrently.
 
@@ -14,3 +14,4 @@ Will segment the download into 10 pieces and download each one concurrently.
 1. Script is too dumb at this point to detect the actual filename you are downloading so just creates a file called downloadefile instead which you will have to rename
 2. Will not work if downloading from an HTTP 1.0 server and does not check for that yet so be warned
 3. You really don't get much feedback from the script other than if you run tail -f downloadedfile-* and still it is pretty useless
+4. Needs to be rewritten in something other than bash because of the various url's that could come as input
--- dl.sh
+++ dl.sh
@@ -1,13 +1,33 @@
 #!/bin/sh
 
-url="$1"
-
-downloadedfilename="downloadedfile"
-
-if [ "$url" == "" ]
+url=$1
+if [ "${url}" == "" ]
 then
 	echo "Provide a url"
 	exit -1
+fi
+
+file=$(basename $(echo $url | sed -e 's/ /_/g'))
+downloadedfilename="$file"
+if [ "" == "$downloadedfilename" ]
+then
+	echo "Could not determine filename. Using downloadedfile as filename"
+	downloadedfilename="downloadedfile"
+fi
+
+curl_path=$(which curl)
+if [ "" == "$curl_path" ]
+then
+	echo "Looks like you are missing curl. You will need to install this"
+	exit 1
+fi
+
+http_ver=$(curl -sI "$url" | grep '^HTTP' | sed -e 's/^HTTP\/\([12]\).*/\1/')
+if [ $http_ver -eq 1 ]
+then
+	echo "HTTP version 1 detected. Can not segment the download"
+	curl "$url" -o ${downloadedfilename}
+	exit 0
 fi
 
 chunks=10
@@ -15,6 +35,7 @@ chunks=10
 length=$(curl -sI "$url" | awk -F':' '/Content-Length/ {print $2}' | egrep -o '[0-9]+')
 chunksize=$(($length / 10))
 
+exit 1
 count=0
 start=0
 end=$chunksize
